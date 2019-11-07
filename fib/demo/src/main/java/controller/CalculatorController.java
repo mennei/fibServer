54907096ea86914a9bed8083c730fadc1f13ad18
mennei <mennei.segal@gmail.com>
2019-11-07 19:14:07 +0200

    Adding server side limitation

--- fib/demo/src/main/java/controller/CalculatorController.java
+++ fib/demo/src/main/java/controller/CalculatorController.java
@@ -15,23 +15,22 @@ public class CalculatorController {
     }
 
     // Calculate fibonacci number with loop
-    private long fib(long n)
-    {
+    private long fib(long n) {
         long n1 = 0, n2 = 1, n3 = 1;
-        if (n == 0){
+        if (n == 0) {
             return n1;
         }
-        if (n == 1){
+        if (n == 1) {
             return n2;
         }
-        for(int i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) {
             n3 = n1 + n2;
-            if (n3 < Long.MAX_VALUE) {
+            long limit = Long.MAX_VALUE - n2;
+            if (n3 < limit) {
                 n1 = n2;
                 n2 = n3;
-            }
-            else {
-                return  Long.MAX_VALUE;
+            } else {
+                throw new UnsupportedOperationException("Result number is too big");
             }
         }
         return n3;
